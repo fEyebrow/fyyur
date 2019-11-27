ad7695556c170718c4653398066fc6d042f33292
四条眉毛 <fourfishxiake@gmail.com>
2019-11-28 00:05:39 +0800

    separate models

--- app.py
+++ app.py
@@ -17,71 +17,20 @@ import sys
 from itertools import groupby
 from datetime import datetime
 from util import *
+from models import Show, Venue, Artist, db
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
-
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
-
+db.init_app(app)
+with app.app_context():
+        db.create_all()
+        
 migrate = Migrate(app, db)
 
-class Show(db.Model):
-    __tablename__ = 'show'
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
 
-class Venue(db.Model):
-  
-    __tablename__ = 'venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    genres = db.Column(db.ARRAY(db.String()))
-    address = db.Column(db.String(120), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-
-    shows = db.relationship('Show', backref=db.backref('venue', lazy=True))
-    def __init(self,name,genres,address,city,state,phone,facebook_link):
-        self.name = name
-        self.genres = genres
-        self.address = address
-        self.city = city
-        self.state = state
-        self.phone = phone
-        self.facebook_link = facebook_link
-    def __repr__(self): 
-        return f'<venue {self.id} {self.name}>'
-    
-class Artist(db.Model):
-    __tablename__ = 'artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String,nullable=False)
-    city = db.Column(db.String(120),nullable=False)
-    state = db.Column(db.String(120),nullable=False)
-    phone = db.Column(db.String(120),nullable=False)
-    genres = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
-
-    shows = db.relationship('Show', backref=db.backref('artist', lazy=True))
-
-
-db.create_all()
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
--- models.py
+++ models.py
@@ -1,8 +1,5 @@
-
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class Show(db.Model):
     __tablename__ = 'show'
